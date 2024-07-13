bc2c3b072527d374b6850ba99f9ace14eace352c
Tem Revil <temrevil@gmail.com>
2024-07-13 11:08:54 +0300

    Aside Menu Event

--- JavaScipt/Swing.js
+++ JavaScipt/Swing.js
@@ -1,4 +1,4 @@
-// Login/Logou Event
+// Logout Event
 const modal = document.querySelector('.modal.logout-modal');
 
 document.getElementById('logout-modal').addEventListener('click', () => {
@@ -20,3 +20,75 @@ document.addEventListener('click', (e) => {
   }
 });
 // -----------------------------------------
+// Menu Kinds Slider
+const menuKinds = document.querySelector('.menu-kinds');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+menuKinds.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX;
+  scrollLeft = menuKinds.scrollLeft;
+});
+
+menuKinds.addEventListener('touchstart', (e) => {
+  isDown = true;
+  startX = e.touches[0].pageX;
+  scrollLeft = menuKinds.scrollLeft;
+});
+
+menuKinds.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  const x = e.pageX;
+  const walk = x - startX;
+  menuKinds.scrollLeft = scrollLeft - walk;
+});
+
+menuKinds.addEventListener('touchmove', (e) => {
+  if (!isDown) return;
+  const x = e.touches[0].pageX;
+  const walk = x - startX;
+  menuKinds.scrollLeft = scrollLeft - walk;
+});
+
+menuKinds.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+menuKinds.addEventListener('touchend', () => {
+  isDown = false;
+});
+
+const kindBoxes = menuKinds.querySelectorAll('.kind-box');
+
+kindBoxes.forEach((kindBox) => {
+  kindBox.addEventListener('click', () => {
+    kindBoxes.forEach((kb) => kb.classList.remove('active'));
+    kindBox.classList.add('active');
+  });
+});
+// -----------------------------------------
+// Aside Buttons Gif Animated
+// Get all buttons with the class "aside-button"
+const buttons = document.querySelectorAll('.aside-button');
+
+// Add an event listener to each button
+buttons.forEach(button => {
+  button.addEventListener('click', event => {
+    // Get the button element (not the img element)
+    const buttonElement = event.currentTarget;
+
+    // Get the img element inside the button
+    const img = buttonElement.querySelector('img');
+
+    // Change the src attribute from .png to .gif
+    const gifSrc = img.src.replace('.png', '.gif');
+    img.src = gifSrc;
+
+    // Change the src attribute back to .png after 0.4 seconds
+    setTimeout(() => {
+      img.src = img.src.replace('.gif', '.png');
+    }, 1300);
+  });
+});
