84ea46eda436a4c286d12eb92dcaea4482f59a10
Tem Revil <temrevil@gmail.com>
2024-07-22 17:56:54 +0300

    Expanding Swing.js

---
+++ JavaScript/Swing-Base.js
@@ -0,0 +1,53 @@
+import { db, doc, getDoc, deleteDoc, setDoc, updateDoc } from './Firebase.js';
+
+// -----------------------------------------
+// Auth Guardian Helper
+document.getElementById('logout-yes').addEventListener('click', logout);
+
+async function logout() {
+  const username = localStorage.getItem('username');
+  const deviceName = localStorage.getItem('device');
+  const loginAuthRef = doc(db, 'LoginAuth', `Login-Code-${username}`);
+  const dailyLoginRef = doc(db, 'DailyLogin', `${username}/${new Date().getMonth() + 1} - ${new Date().toLocaleString('en-US', { month: 'long' })}`, `${new Date().getDate()} - ${new Date().toLocaleString('en-US', { weekday: 'long' })}`);
+
+  try {
+    if (deviceName) {
+      // حذف الحقل الخاص بالجهاز
+      await updateDoc(loginAuthRef, {
+        [deviceName]: deleteDoc()
+      });
+      localStorage.removeItem('device');
+    }
+
+    localStorage.removeItem('username');
+
+    // Add logout time and count to the daily login document
+    const dailyLoginSnap = await getDoc(dailyLoginRef);
+    if (dailyLoginSnap.exists()) {
+      await setDoc(dailyLoginRef, {
+        loginCount: dailyLoginSnap.data().loginCount,
+        loginTimes: dailyLoginSnap.data().loginTimes,
+        logoutCount: (dailyLoginSnap.data().logoutCount || 0) + 1,
+        logoutTimes: [...(dailyLoginSnap.data().logoutTimes || []), new Date().toLocaleTimeString()],
+      }, { merge: true });
+    } else {
+      await setDoc(dailyLoginRef, {
+        loginCount: 0,
+        loginTimes: [],
+        logoutCount: 1,
+        logoutTimes: [new Date().toLocaleTimeString()],
+      });
+    }
+
+    window.location.href = 'Login.html';
+  } catch (error) {
+    console.error('Error deleting device field:', error);
+  }
+}
+// -----------------------------------------
+// Nav Greeting Message
+window.addEventListener('load', () => {
+  const username = localStorage.getItem('username');
+  const greetingElement = document.getElementById('greeting');
+  greetingElement.textContent = `Hello, ${username}!`;
+});
